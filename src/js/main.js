25ffaede2bad4251e711131912d6c5b687d5e2f7
jakubGora <jakub_gora@wp.pl>
2022-06-01 12:09:43 +0200

    Styling and click events

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+const articleBtn = document.getElementsByClassName("article-btn")[0];
+const closeBtn = document.getElementsByClassName("close-btn")[0];
+const alertWin = document.getElementsByClassName("alert")[0];
+const clickN = document.getElementById("nOfClicks");
+const zerowanieBtn = document.getElementsByClassName("setNull-btn")[0];
+//localStorage.setItem("clicked", 0);
+
+zerowanieBtn.addEventListener("click", () => {
+  localStorage.setItem("clicked", 0);
+  clickN.innerHTML = 0;
+});
+
+articleBtn.addEventListener("click", () => {
+  alertWin.style.display = "block";
+  localStorage.setItem(
+    "clicked",
+    Number(localStorage.getItem("clicked")) + 1 || 1
+  );
+  clickN.innerHTML = localStorage.getItem("clicked")?.toString() || 1;
+  if (Number(localStorage.getItem("clicked")) > 5) {
+    zerowanieBtn.style.display = "block";
+  } else zerowanieBtn.style.display = "none";
+});
+
+closeBtn.addEventListener("click", () => {
+  alertWin.style.display = "none";
+});
