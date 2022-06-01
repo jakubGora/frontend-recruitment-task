cfaa5e0cf1904fb50baafb739fb7c3c49c44cdb0
jakubGora <jakub_gora@wp.pl>
2022-06-01 21:38:12 +0200

    Clearing the code

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,12 @@ const closeBtn = document.getElementsByClassName("close-btn")[0];
 const alertWin = document.getElementsByClassName("alert")[0];
 const alertWinContent = document.getElementsByClassName("content")[0];
 const clickN = document.getElementById("nOfClicks");
-const zerowanieBtn = document.getElementsByClassName("setNull-btn")[0];
+const resetBtn = document.getElementsByClassName("setNull-btn")[0];
 
-zerowanieBtn.addEventListener("click", () => {
+resetBtn.addEventListener("click", () => {
   localStorage.setItem("clicked", 0);
   clickN.innerHTML = 0;
-  zerowanieBtn.style.display = "none";
+  resetBtn.style.display = "none";
 });
 
 articleBtn.addEventListener("click", () => {
@@ -17,10 +17,10 @@ articleBtn.addEventListener("click", () => {
     "clicked",
     Number(localStorage.getItem("clicked")) + 1 || 1
   );
-  clickN.innerHTML = localStorage.getItem("clicked")?.toString() || 1;
-  if (Number(localStorage.getItem("clicked")) > 5) {
-    zerowanieBtn.style.display = "block";
-  } else zerowanieBtn.style.display = "none";
+  clickN.innerHTML = localStorage.getItem("clicked") || 1;
+  if (Number(localStorage.getItem("clicked")) > 5)
+    resetBtn.style.display = "block";
+  else resetBtn.style.display = "none";
 });
 
 closeBtn.addEventListener("click", () => {
@@ -28,7 +28,7 @@ closeBtn.addEventListener("click", () => {
 });
 
 alertWin.addEventListener("click", function (event) {
-  var isClickInsideElement = alertWinContent.contains(event.target);
+  let isClickInsideElement = alertWinContent.contains(event.target);
   if (!isClickInsideElement) {
     alertWin.style.display = "none";
   }
