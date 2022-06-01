b53e3aca1b65fa9eec9a0311daebc88c57edb245
jakubGora <jakub_gora@wp.pl>
2022-06-01 16:24:22 +0200

    Button efect

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 const articleBtn = document.getElementsByClassName("article-btn")[0];
 const closeBtn = document.getElementsByClassName("close-btn")[0];
 const alertWin = document.getElementsByClassName("alert")[0];
+const alertWinContent = document.getElementsByClassName("content")[0];
 const clickN = document.getElementById("nOfClicks");
 const zerowanieBtn = document.getElementsByClassName("setNull-btn")[0];
 
 zerowanieBtn.addEventListener("click", () => {
   localStorage.setItem("clicked", 0);
   clickN.innerHTML = 0;
+  zerowanieBtn.style.display = "none";
 });
 
 articleBtn.addEventListener("click", () => {
@@ -23,4 +25,11 @@ articleBtn.addEventListener("click", () => {
 
 closeBtn.addEventListener("click", () => {
   alertWin.style.display = "none";
+});
+
+alertWin.addEventListener("click", function (event) {
+  var isClickInsideElement = alertWinContent.contains(event.target);
+  if (!isClickInsideElement) {
+    alertWin.style.display = "none";
+  }
 });
